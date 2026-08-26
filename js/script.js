2f774b8e91ae682b3d3185f725647aa94e81bcc1
isabeliguedes <isabeli_lemos@estudante.sesisenai.org.br>
2026-08-26 20:51:56 -0300

    Implement user login validation
    Added user validation for login form with predefined credentials.

--- js/script.js
+++ js/script.js
@@ -8,3 +8,36 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+const usuariosValidos = {
+    "admin": "admin123",
+    "joao": "senha123",
+    "maria": "maria2014"
+};
+
+
+var loginForm = document.querySelector('#id01 form');
+var modal = document.querySelector('#id01');
+
+
+loginForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+
+   
+    var usuario = loginForm.usuario.value.trim();
+    var senha = loginForm.senha.value;
+
+
+   
+    if (usuariosValidos.hasOwnProperty(usuario) && usuariosValidos[usuario] === senha) {
+        alert("Sucesso!!!");
+       
+       
+        if (modal) modal.style.display = "none";
+        loginForm.reset();
+    } else {
+        alert("Usuário ou senha incorretos :(");
+    }
+});
+
+
